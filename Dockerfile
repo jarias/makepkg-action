9d0d4f336aa3671a59187161ab2ed35d9d4e45af
Julio Arias <jarias@users.noreply.github.com>
2020-07-13 19:14:51 -0600

    chore: added menari pacman repo

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ FROM archlinux
 RUN pacman --noconfirm -Sy base-devel aws-cli
 RUN useradd build -m
 RUN echo 'build ALL=NOPASSWD: ALL' >> /etc/sudoers
+RUN echo '[menari]' >> /etc/pacman.conf
+RUN echo 'Server = https://s3.amazonaws.com/arch.menari.io/repo/x86_64' >> /etc/pacman.conf
+RUN echo 'SigLevel = Never' >> /etc/pacman.conf
+RUN pacman --noconfirm -Syu
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,12 @@ sudo -u build sh -c "echo 'allow-preset-passphrase' >> ~/.gnupg/gpg-agent.conf"
 sudo -u build sh -c "gpg-connect-agent RELOADAGENT /bye"
 sudo -u build sh -c "gpg-connect-agent 'PRESET_PASSPHRASE $GPG_KEY_GRIP -1 $GPG_KEY_PASSPHRASE' /bye"
 sudo -u build sh -c "makepkg --sign --syncdeps --noconfirm"
+ls -l
 aws configure list
 mkdir -p local-repo
 aws s3 sync --follow-symlinks s3://$BUCKET/repo/x86_64/ local-repo/
 chmod -R a+rw local-repo
-sudo -u build sh -c "repo-add -s local-repo/$REPO.db.tar.gz *.pkg.tar.xz"
+sudo -u build sh -c "repo-add -s local-repo/$REPO.db.tar.gz *.pkg.tar.zst"
+find . -maxdepth 1 -name '*.pkg.tar.zst' | xargs -I {} cp {} local-repo/
+find . -maxdepth 1 -name '*.pkg.tar.zst.sig' | xargs -I {} cp {} local-repo/
 aws s3 sync --follow-symlinks --acl public-read local-repo/ s3://$BUCKET/repo/x86_64/
-aws s3 cp *.pkg.tar.xz s3://$BUCKET/repo/x86_64/
-aws s3 cp *.pkg.tar.xz.sig s3://$BUCKET/repo/x86_64/
